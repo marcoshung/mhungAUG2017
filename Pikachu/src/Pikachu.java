dc2f360125db1dea62e0549cb8f5fad3fec5020d
marcoshung <mhung927@gmail.com>
2017-09-06 09:51:50 -0700

    pik

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,10 @@
+public class Pikachu {
+	public static void main(String[]args){
+		System.out.println("    Pikachu Welcomes you to the world of "
+				+ "Pokemon!");
+		System.out.println("           (\\__/)       ");
+		System.out.println("           (o^.^)         ");
+		System.out.println("        z(_(\")(\")    ");
+		
+		}
+}
