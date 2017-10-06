fd02ec84857efbe9b12755f4874e49701a7a8d69
marcoshung <mhung927@gmail.com>
2017-10-06 09:50:59 -0700

    WSD

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,7 +1,10 @@
 import java.util.*;
 public class QuadraticClient {
 	public static void main(String [] args) {
+		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to the Quadratic Describer\nProvide values for the coefficients a, b, c\na:");
+		double a = input.nextDouble();
+		System.out.println("b);
 	}
 
 }
