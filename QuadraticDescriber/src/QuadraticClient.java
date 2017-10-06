74c73b5b33e3ff035db7cf10c9b5163fbb7c83cc
marcoshung <mhung927@gmail.com>
2017-10-06 09:49:06 -0700

    quadratic

---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class QuadraticClient {
+	public static void main(String [] args) {
+		System.out.println("Welcome to the Quadratic Describer\nProvide values for the coefficients a, b, c\na:");
+	}
+
+}
