9a93103c163af496b6673e4b3461c74eced3c909
marcoshung <mhung927@gmail.com>
2017-10-10 10:28:40 -0700

    done

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,4 +1,70 @@
 
 public class Quadratic {
-
+	public static double sqrt(double operand) {
+		if(operand < 0) {
+			throw new IllegalArgumentException("Cannot square root negatives");
+		}else if(operand == 0) {
+			return 0;
+		}
+		double numOne = 0;
+		while(numOne * numOne <operand) {
+			numOne++;
+		}
+		if((numOne *numOne) % operand == 0) {
+			return numOne;
+		}else {
+			return .5 * (operand/numOne + numOne);
+		}
+	}
+	public static double discriminant(double a, double b, double c) {
+		return b*b - (4*a*c);
+	}
+	public static double round2(double operand) {
+		if(operand >0) {
+			double newOperand = operand*100 + .5;
+			int num = (int) (newOperand);
+			return ((double) (num))/100;
+		}else {
+			double newOperand = operand*100 - .5;
+			int num = (int) (newOperand);
+			return ((double) (num))/100;
+		}
+	}
+	public static String quadForm(double a, double b, double c) {
+		double discriminant = discriminant(a,b,c);
+		if(discriminant < 0) {
+			return ("no real roots");
+		}else {
+			if(a == 0) {
+				throw new IllegalArgumentException("Not a quadratic equation");
+			}
+			double rootOne = (-b+ sqrt(discriminant))/(2*a);
+			double rootTwo = (-b- sqrt(discriminant))/(2*a);
+			if(rootOne != rootTwo) {
+				rootOne = round2(rootOne);
+				rootTwo = round2(rootTwo);
+				if(rootOne > rootTwo) {
+					return rootTwo + " and " + rootOne;
+				}else {
+					return rootOne + " and " + rootTwo;
+				}
+			}else {
+				rootOne = round2(rootOne);
+				return rootOne +"";
+			}
+		}
+	}
+	public static double exponent(double operand, int exponent) {
+		if(exponent < 0) {
+			throw new IllegalArgumentException("Cannot take negative exponents");
+		}
+		if(exponent ==0) {
+			return 1;
+		}
+		double multiply = operand;
+		for(int i = 1; i<exponent;i++) {
+			operand *= multiply;
+		}
+		return operand;
+	}
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -2,9 +2,47 @@ import java.util.*;
 public class QuadraticClient {
 	public static void main(String [] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Welcome to the Quadratic Describer\nProvide values for the coefficients a, b, c\na:");
-		double a = input.nextDouble();
-		System.out.println("b);
+		System.out.println("Welcome to the Quadratic Describer\nProvide values for the coefficients a, b, c");
+		boolean done = false;
+			while(!done) {
+			System.out.println("a:");
+			double a = input.nextDouble();
+			System.out.println("b:");
+			double b = input.nextDouble();
+			System.out.println("c:");
+			double c = input.nextDouble();
+			System.out.println(quadDescriber(a,b,c));
+			System.out.println("Do you want to keep going? <Type \"quit\" to end>");
+			String finished = input.next();
+			if(finished.equals("done")) {
+				done = true;
+			}
+		}
+	}
+	public static String quadDescriber(double a, double b, double c) {
+		String summary = "Description of the graph of:\ny = " + a +"x^2 + " + b + "x + " + c +"\n\n";
+		double yInt=c;
+		//gives direction of graph
+		if(a > 0) {
+			summary += "Opens: up \n";
+		}else if(a <0) {
+			summary += "Opens: down \n";
+		}
+		//axis of symmetry
+		double axis = -b/(2*a);
+		summary += "Axis of Symmetry: " + Quadratic.round2(axis) +"\n";
+		//vertex
+		double yValue= (a * Quadratic.exponent(axis, 2)) + (b *axis) + c;
+		summary += "Vertex: <" + axis + "," +Quadratic.round2(yValue) + ">\n";
+		//roots
+		String roots = Quadratic.quadForm(a,b,c);
+		if(roots.equals("no real roots")) {
+			roots = "None";
+		}
+		summary += "x-intercepts : " + roots +"\n";
+		//y-intercept
+		summary += "y-intercept : " + c + "\n\n";
+		return summary;
 	}
 
 }
