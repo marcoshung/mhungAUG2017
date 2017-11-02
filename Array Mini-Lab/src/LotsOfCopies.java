e6ae559a8f9255cc63737c528c1c85e1762065c6
marcoshung <mhung927@gmail.com>
2017-11-02 09:39:13 -0700

    fixed

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -5,35 +5,28 @@ public class LotsOfCopies {
 		int num = 7;
 		String strMain = "APCS";
 		int[] arrMain = {1,2,3,4,5};
-		LotsOfCopies.changeMe(num,strMain,arrMain);
+		changeMe(num,strMain,arrMain);
 		System.out.println(num);
 		System.out.println(strMain);
 		System.out.println(Arrays.toString(arrMain));
-		int[] startValue = {1,2,3};
-		int[] newValue = {4,5,6};
-		int[] a = startValue;
-		int[] b =a;
-		a = newValue;
-		System.out.println(Arrays.toString(a));
-		System.out.println(Arrays.toString(b));
-		int num1 = 5;
-		int num2 = 2;
-		num2 = num1;
-		num1 = 9;
-		System.out.println(num1 + " " + num2);
-		String yes = "yes";
-		String no = "no";
-		no = yes;
-		yes = "maybe";
-		System.out.println(yes + " " + no);
-		
 		
 	}
 	public static void changeMe(int x,String str, int[] arr) {
 		x =2;
 		str = "no";
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = i+1;
-		}
+		arr[0] = 10;
+		int num2 = 2;
+		num2 = x;
+		x = 9;
+		System.out.println(x + " " + num2);
+		
+		String no = "no";
+		no = str;
+		str = "maybe";
+		System.out.println(str + " " + no);
+		int[] newValue = arr;
+		arr[0]= 20;
+		System.out.println(Arrays.toString(newValue));
+		System.out.println(Arrays.toString(arr));
 	}
 }
