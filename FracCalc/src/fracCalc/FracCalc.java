8bb901b4328a054325b189934865df45bf9d4ae9
marcoshung <mhung927@gmail.com>
2017-11-08 10:39:47 -0800

    Checkpoint 1 done

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,11 +1,13 @@
 package fracCalc;
-
+import java.util.*;
+import java.util.Arrays;
 public class FracCalc {
-
-    public static void main(String[] args) 
-    {
+	public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a fraction equation");
+		String equation = input.nextLine();
+		System.out.println(produceAnswer(equation));
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -16,11 +18,10 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String equation){ 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+    	String[] fracStore = equation.split(" ");
+        return(fracStore[fracStore.length -1]);
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
