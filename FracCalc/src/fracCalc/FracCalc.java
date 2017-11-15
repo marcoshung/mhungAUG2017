5e5da80f9ba059cd27293098d442c6e516ab8895
marcoshung <mhung927@gmail.com>
2017-11-15 10:33:46 -0800

    chkpoint 2 done

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,3 +1,4 @@
+//Marcos Hung; Period 2
 package fracCalc;
 import java.util.*;
 import java.util.Arrays;
@@ -8,14 +9,14 @@ public class FracCalc {
 		System.out.println("Enter a fraction equation");
 		boolean done = false;
 		while(!done) {
-		String equation = input.nextLine();
-		if(equation.equals("quit")) {
-			done = true;
-		}
-		System.out.println(produceAnswer(equation));
+			String equation = input.nextLine();
+			if(equation.equals("quit")) {
+				done = true;
+			}else {
+				System.out.println(produceAnswer(equation));
+			}
 		}
     }
-    
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
     //
@@ -27,37 +28,38 @@ public class FracCalc {
     public static String produceAnswer(String equation){ 
         // TODO: Implement this function to produce the solution to the input
     	String[] fracStore = equation.split(" ");
-        pa
+    	parse(fracStore[0]);
+    	return parse(fracStore[2]);	
     }
     public static String parse(String num) {
     	int breakPoint = 0;
     	String whole ="";
     	String denominator = "";
     	String numerator = "";
+    	//traverses the number for underscores and backslashes to get numbers accordingly 
     	for(int i = 0;i < num.length(); i ++) {
     		if(num.charAt(i) == '_') {
     			whole = num.substring(0, i);
     			breakPoint = i;
     		}
     		if(num.charAt(i) == '/') {
-    			numerator += num.substring(breakPoint,i);
-    			denominator += num.substring(i,num.length());
-    		}
-    		if(num.indexOf("_") == -1 && num.length() != 0) {
-    			if(num.indexOf("/") == -1) {
-    				whole += num.substring(0,num.length());
-    				denominator = "0";
-    				numerator ="0";
-    			}else {
-    				whole = "0";
-    				denominator =num.substring(0, num.indexOf("/"));
-    				numerator =num.substring(num.indexOf("/"),num.length());
-    			}
+    			numerator += num.substring(breakPoint + 1,i);
+    			denominator += num.substring(i +1,num.length());
     		}
     	}
-    	return ("Whole:" + whole + " numerator:" + numerator + " denominator:" + denominator);
+    	//checks to see if there is an underscore, if not then if there is a backslash it is a whole num, and if there isnt a backslash it's a fraction
+    	if(num.indexOf("_") == -1 && num.length() != 0) {
+    		if(num.indexOf("/") == -1) {
+    			whole = num.substring(0,num.length());
+    			numerator = "0";
+    			denominator ="1";
+    		}else {
+   				whole = "0";
+   				numerator =num.substring(0, num.indexOf("/"));
+   				denominator =num.substring(num.indexOf("/") + 1,num.length());
+   			}
+   		}
+    	return ("whole:" + whole + " numerator:" + numerator + " denominator:" + denominator);
     }
-    
     // TODO: Fill in the space below with any helper methods that you think you will need
-    
 }
