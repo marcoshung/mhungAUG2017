0155c2a2afa634c25c1dd30130c6509deb563acc
Marcos Hung <Marcos Hung@DESKTOP-OUIT3F6>
2017-12-11 22:27:03 -0800

    ffd

--- FracCalcNew/src/fracCalc/FracCalc.java
+++ FracCalcNew/src/fracCalc/FracCalc.java
@@ -3,7 +3,17 @@ import java.util.*;
 public class FracCalc {
     public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
-    	
+    	Scanner input = new Scanner(System.in);
+		System.out.println("Enter a fraction equation");
+		boolean done = false;
+		while(!done) {
+			String equation = input.nextLine();
+			if(equation.equals("quit")) {
+				done = true;
+			}else {
+				System.out.println(produceAnswer(equation));
+			}
+		}
     }
     public static String produceAnswer(String equation){ 
         String[] fracStore = equation.split(" ");
@@ -14,8 +24,8 @@ public class FracCalc {
     	//these arrays will be used to hold the numerator and denominator of the fractions
     	int[] storeNum1 = new int[2];
     	int[] storeNum2 = new int[2];
-    	Fraction fracOne = new Fraction(fracStore[0], storeNum1);
-    	Fraction fracTwo = new Fraction(fracStore[2], storeNum2);
+    	Fraction fracOne = new Fraction(fracStore[0]);
+    	Fraction fracTwo = new Fraction(fracStore[2]);
         return "";
     }
 }
--- FracCalcNew/src/fracCalc/Fraction.java
+++ FracCalcNew/src/fracCalc/Fraction.java
@@ -1,52 +1,48 @@
-package fracCalc;
-
-public class Fraction {
-	//Marcos Hung; Per 2
-	package fracCalc;
-	public class Fraction {
-		private int numerator;
-		private int denominator;
-		private int sign;
-		
-		public Fraction(String num, int[] array) {
-			int breakPoint = 0;
-		    String whole ="";
-		   	String denominator = "";
-		  	String numerator = "";
-		   	//traverses the number for underscores and backslashes to get numbers accordingly 
-		   	for(int i = 0;i < num.length(); i ++) {
-		  		if(num.charAt(i) == '_') {
-		   			whole = num.substring(0, i);
-		   			breakPoint = i;
-	    		}
-		  		if(num.charAt(i) == '/') {
-		    		numerator += num.substring(breakPoint + 1,i);
-		    		denominator += num.substring(i +1,num.length());
-		   		}
-		   	}
-		   	//checks to see if there is an underscore, if not then if there isn't a backslash it is a whole num, and if there is a backslash it's a fraction
-		   	if(num.indexOf("_") == -1 && num.length() != 0) {
-		   		if(num.indexOf("/") == -1) {	   			
-		   		whole = num.substring(0,num.length());
-	   			numerator = "0";
-	    		denominator ="1";
-	    	}else {
-		   		whole = "0";
-		   		numerator =num.substring(0, num.indexOf("/"));
-	 			denominator =num.substring(num.indexOf("/") + 1,num.length());	  			}
-		   	}
-		   	if(Integer.parseInt(whole) < 0 || Integer.parseInt(numerator) < 0) {
-		   		this.sign = -1;
-		   	}else {
-		   		this.sign = 1;
-		   	}
-		   	//checks to see if whole num is negative, if it is then the numerator will be subtracted to get more negative
-		   	if(this.sign == 1 || whole.equals("0")) {
-		   		this.numerator = Integer.parseInt(whole) * Integer.parseInt(denominator) + Integer.parseInt(numerator);
-		    }else {
-	    		this.numerator = Integer.parseInt(whole) * Integer.parseInt(denominator) - Integer.parseInt(numerator);
-	    	}
-		   	this.denominator = Integer.parseInt(denominator);
+//Marcos Hung; Per 2
+public class Fraction{
+	private int numerator;
+	private int denominator;
+	private int sign;
+	public Fraction(String num) {
+		int breakPoint = 0;
+		int[] array = new int[2];
+		String whole ="";
+		String denominator = "";
+		String numerator = "";
+		//traverses the number for underscores and backslashes to get numbers accordingly 
+		for(int i = 0;i < num.length(); i ++) {
+	  		if(num.charAt(i) == '_') {
+	  			whole = num.substring(0, i);
+	 			breakPoint = i;
+	   		}
+	  		if(num.charAt(i) == '/') {
+	    		numerator += num.substring(breakPoint + 1,i);
+	    		denominator += num.substring(i +1,num.length());
+	   		}
+	   	}
+		//checks to see if there is an underscore, if not then if there isn't a backslash it is a whole num, and if there is a backslash it's a fraction
+		if(num.indexOf("_") == -1 && num.length() != 0) {
+			if(num.indexOf("/") == -1) {	   			
+			whole = num.substring(0,num.length());
+	   		numerator = "0";
+	    	denominator ="1";
+	    }else {
+			whole = "0";
+			numerator =num.substring(0, num.indexOf("/"));
+	 		denominator =num.substring(num.indexOf("/") + 1,num.length());	  			}
 		}
+		if(Integer.parseInt(whole) < 0 || Integer.parseInt(numerator) < 0) {
+		  	this.sign = -1;
+		}else {
+			this.sign = 1;
+		}
+		//checks to see if whole num is negative, if it is then the numerator will be subtracted to get more negative
+		if(this.sign == 1 || whole.equals("0")) {
+			this.numerator = Integer.parseInt(whole) * Integer.parseInt(denominator) + Integer.parseInt(numerator);
+		 }else {
+			 this.numerator = Integer.parseInt(whole) * Integer.parseInt(denominator) - Integer.parseInt(numerator);
+	    }
+	  	this.denominator = Integer.parseInt(denominator);
 	}
+	
 }
