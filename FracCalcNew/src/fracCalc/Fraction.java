244e181b79881504802639ba9a482a302962c7ee
marcoshung <mhung927@gmail.com>
2017-12-13 09:51:25 -0800

    chkpoint 2

--- FracCalcNew/src/fracCalc/Fraction.java
+++ FracCalcNew/src/fracCalc/Fraction.java
@@ -8,18 +8,10 @@ public class Fraction{
 	private int whole;
 	private int[] storeNum = new int[2];
 	public Fraction(String num) {
+		String numerator;
+		String denominator;
     	int breakPoint = 0;
-    	//traverses the number for underscores and backslashes to get numbers accordingly 
-    	for(int i = 0;i < num.length(); i ++) {
-    		if(num.charAt(i) == '_') {
-    			this.whole = Integer.parseInt(num.substring(0, i));
-    			breakPoint = i;
-    		}
-    		if(num.charAt(i) == '/') {
-    			this.numerator = Integer.parseInt(num.substring(breakPoint + 1,i));
-    			this.denominator = Integer.parseInt(num.substring(i +1,num.length()));
-    		}
-    	}
+    	
     	//checks to see if there is an underscore, if not then if there isn't a backslash it is a whole num, and if there is a backslash it's a fraction
     	if(num.indexOf("_") == -1 && num.length() != 0) {
     		if(num.indexOf("/") == -1) {
@@ -31,6 +23,18 @@ public class Fraction{
    				this.numerator =Integer.parseInt(num.substring(0, num.indexOf("/")));
    				this.denominator =Integer.parseInt(num.substring(num.indexOf("/") + 1,num.length()));
    			}
+    	}else {
+    	//traverses the number for underscores and backslashes to get numbers accordingly 
+    		for(int i = 0;i < num.length(); i ++) {
+    			if(num.charAt(i) == '_') {
+    				this.whole = Integer.parseInt(num.substring(0, i));
+    				breakPoint = i;
+    			}
+    			if(num.charAt(i) == '/') {
+    				this.numerator = Integer.parseInt(num.substring(breakPoint + 1,i));
+    				this.denominator = Integer.parseInt(num.substring(i + 1,num.length()));
+    			}
+    		}
     	}
     	//checks to see if whole num is negative, if it is then the numerator will be subtracted to get more negative
     }
