ff80c9d9a284826137800a6b44b9ae4b0ca3e89d
marcoshung <mhung927@gmail.com>
2018-02-14 10:08:51 -0800

    dsa

---
+++ Calculate Library/src/CalcuRunner.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CalcuRunner {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		private boolean done = false;
+		while(!done) {
+			System.out.println("Enter an equation");
+			String equation = input.nextLine();
+			if(equation.equals("quit")) {;
+				done = true;
+			}eles{
+				Calculate.produceAnswer(equation))
+			}
+		}
+	}
+}
