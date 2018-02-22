1b7c596f50fe0c464a35bcf2b97fa033d2421398
marcoshung <mhung927@gmail.com>
2018-02-22 09:50:00 -0800

    naskfd

--- String Sorter/src/Runner.java
+++ String Sorter/src/Runner.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Arrays;
 public class Runner {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -14,18 +15,43 @@ public class Runner {
 		}
 	}
 	public static String produceAnswer(String sentence) {
-		String[] array = sentence.split(" ");
+		ArrayList<String> array = new ArrayList<>(Arrays.asList(sentence.split(" ")));
 		String sorted = "";
-		for(int i = 0; i < array.length; i++) {
-			sorted += sort(array[i]) + " ";
+		for(int i = 0; i < array.size(); i++) {
+			sorted += sort(array.get(i)) + " ";
 		}
 		return sorted;
 	}
 	public static String sort(String word) {
-		char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-		ArrayList<> newWord= new ArrayList<char>();
-		for(int i = 0; i < word.length();i++) {
-			
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		char[] newWord = new char[word.length()];
+		if(word.length() == 1) {
+			return word.substring(0);
+		}else if(word.length() <= 0) {
+			return "";
 		}
+		newWord[0] = word.charAt(0);
+		for(int i = 1; i < word.length();i++) {
+			if(alphabet.indexOf(word.charAt(i)) < alphabet.indexOf(word.charAt(i-1))) {
+				for(int j = 0; i< word.substring(0,i).length(); i++) {
+					if(alphabet.indexOf(word.charAt(i -j)) < alphabet.indexOf(word.charAt(i-(j+1)))) {
+						newWord[i-1] = word.charAt(i);
+						newWord[i] = word.charAt(i-1);
+					}else {
+						j = word.substring(0,i).length();
+					} 
+				}
+			}else {
+				newWord[i] = word.charAt(i);
+			}
+		}
+		return(toString(newWord));
+	}
+	public static String toString(char[] array) {
+		String newWord = "";
+		for(int i = 0; i<array.length; i++) {
+			newWord += array[i];
+		}
+		return newWord;
 	}
 }
