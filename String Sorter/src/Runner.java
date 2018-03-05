87fd359259f1256d1db5725d1a310740be51338d
marcoshung <mhung927@gmail.com>
2018-03-05 09:47:59 -0800

    dfsd

--- String Sorter/src/Runner.java
+++ String Sorter/src/Runner.java
@@ -23,46 +23,10 @@ public class Runner {
 		return sorted;
 	}
 	public static String sort(String word) {
-		String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 		String sortedWord = "";
-		for(int i = 0; i < word.length();i++) {
-			for(int j = 0; j < alphabet.length; i++) {
-				String letter = alphabet[i];
-				if(word.charAt(i) == (alphabet[j].charAt(0))) {
-					alphabet[i] +=letter;
-					j = alphabet.length;
-				}
-			}
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		char[] newWord = new char[word.length()];
-		if(word.length() == 1) {
-			return word.substring(0);
-		}else if(word.length() <= 0) {
-			return "";
-		}
-		for(int i = 0; i < alphabet.length(); i++) {
-			for(int j = 1; j < alphabet[j].length(); j ++) {
-				sortedWord += String.alphabet[i].charAt(j);
-			}
-		}
-		return sortedWord;
-		newWord[0] = word.charAt(0);
-		for(int i = 1; i < word.length();i++) {
-			if(alphabet.indexOf(word.charAt(i)) < alphabet.indexOf(word.charAt(i-1))) {
-				for(int j = 0; i< word.substring(0,i).length(); i++) {
-					if(alphabet.indexOf(word.charAt(i -j)) < alphabet.indexOf(word.charAt(i-(j+1)))) {
-						newWord[i-1] = word.charAt(i);
-						newWord[i] = word.charAt(i-1);
-					}else {
-						j = word.substring(0,i).length();
-					} 
-				}
-			}else {
-				newWord[i] = word.charAt(i);
-			}
+		for(int i = 0; i < word.length(); i++) {
 		}
 		return(toString(newWord));
-		}
 	}
 	public static String toString(char[] array) {
 		String newWord = "";
