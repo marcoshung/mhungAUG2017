36e539eb4ddff9046a700075240503c405464501
marcoshung <mhung927@gmail.com>
2018-04-20 09:38:41 -0700

    learn graphics

---
+++ Graphic Sandbox/src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		draw();
+	}
+	
+	public void draw() {
+		Graphics.drawString("gello",10,10);
+	}
+}
