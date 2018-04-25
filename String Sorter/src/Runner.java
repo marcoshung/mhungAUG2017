7ebcc4bccd3886b38b3bf6ea1ed71c9e110d09f4
marcoshung <mhung927@gmail.com>
2018-04-25 11:42:17 -0700

    dsa

--- String Sorter/src/Runner.java
+++ String Sorter/src/Runner.java
@@ -2,37 +2,45 @@ import java.util.*;
 import java.util.Arrays;
 public class Runner {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		Scanner console = new Scanner(System.in);
 		System.out.println("Enter a string");
 		boolean done = false;
 		while(!done) {
-			String sentence = input.nextLine();
-			if(sentence.equals("quit")) {
+			String input = console.nextLine();
+			if(input.equals("quit")) {
 				done = true;
 			}else {
-				System.out.println(produceAnswer(sentence));
+				System.out.println(produceAnswer(input));
 			}
 		}
 	}
-	public static String produceAnswer(String sentence) {
-		ArrayList<String> array = new ArrayList<>(Arrays.asList(sentence.split(" ")));
+	public static String produceAnswer(String word) {
+		ArrayList<Character> array = new ArrayList<>();
+		for(int i = 0; i < word.length(); i++) {
+			array.add(word.charAt(i));
+		}
+		return sort(array);
+	}
+	public static String sort(ArrayList<Character> unsortedWord) {
+		ArrayList <Character> sortedWord = new ArrayList<Character>();
+		int sortLimit = unsortedWord.size();
+		ArrayList<Character> sorted = new ArrayList<Character>();
+		for(int i = 0; i < unsortedWord.size(); i++) {
+			char temp = unsortedWord.get(i);
+			for(int j = i + 1; j < unsortedWord.size(); j++) {
+				if(unsortedWord.get(j) < temp) {
+					temp = unsortedWord.get(j);
+				}
+			}
+			sortedWord.add(temp);
+		}
+		return(toString(sortedWord));
+	}
+	public static String toString(ArrayList<Character> array) {
 		String sorted = "";
-		for(int i = 0; i < array.size(); i++) {
-			sorted += sort(array.get(i)) + " ";
+		for(int i = 0; i < array.size(); i ++) {
+			sorted += array.get(i);
 		}
 		return sorted;
-	}
-	public static String sort(String word) {
-		String sortedWord = "";
-		for(int i = 0; i < word.length(); i++) {
-		}
-		return(toString(newWord));
-	}
-	public static String toString(char[] array) {
-		String newWord = "";
-		for(int i = 0; i<array.length; i++) {
-			newWord += array[i];
-		}
-		return newWord;
 	}
 }
