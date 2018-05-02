99b8fc35921e3fcc759708cf7dd7c24393a88c6f
marcoshung <mhung927@gmail.com>
2018-05-02 10:40:48 -0700

    sdf

--- Stats Calcutator/src/StatsRunner.java
+++ Stats Calcutator/src/StatsRunner.java
@@ -1,7 +1,13 @@
 //Marcos Hung
 //This project will be able to calculate simple statistic problems
+import java.util.*;
 public class StatsRunner {
 	public static void main(String[] args) {
-		
+		Scanner console = new Scanner(System.in);
+		boolean done = false;
+		while(!done) {
+			String input = console.nextLine();
+			System.out.println(console.nextLine());
+		}
 	}
 }
