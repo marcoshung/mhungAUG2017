ae24a01f463d945f6d27e1349362b89148f91bf5
Marcos Hung <mhung927@gmail.com>
2019-09-27 23:52:27 -0700

    Added new feature. File Reading

--- Linear Regression/src/Console.java
+++ Linear Regression/src/Console.java
@@ -1,6 +1,11 @@
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import javax.swing.JLabel;
 import javax.swing.JFrame;
 public class Console {
@@ -9,26 +14,42 @@ public class Console {
 		static int height = screenSize.height;
 		static int width = screenSize.width;
 		
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		Scanner console = new Scanner(System.in);
-		System.out.println("This program will calulate the linear regression line given data points\nPlease enter data points and enter DONE to finish");
-		boolean done = false;
-		while(!done) {
-			String input = console.nextLine().toLowerCase();
-			if(input.equals("done")) {
-				done = true;
-			}else if(input.equals("undo")){
-				DataPoints.undo();
-				System.out.println("Your last entry has been removed");
-			}else if(input.equals("test")){
-				DataPoints.useTestData();
-				done = true;
-			}else {
-				parse(input);
+		System.out.println("This program will calulate the linear regression line given data points\nWould you like to read data points from a file or manually input? Enter \"file\" or \"manual\"\n");
+		String input = console.nextLine().toLowerCase();
+		
+		if(input.equals("file")) {
+			System.out.println("Enter the name of the file you want to read from");
+			String fileName = console.nextLine();
+			Scanner in = new Scanner (new File (fileName)); 
+			while(in.hasNext()) {
+				String dataLine = in.nextLine();
+				parse(dataLine);
+			}
+
+		}else if(input.equals("manual")) {
+			boolean done = false;
+			System.out.println("Please enter data points and enter DONE to finish or enter test to use test cases");
+
+			while(!done) {
+				input = console.nextLine().toLowerCase();
+				if(input.equals("done")) {
+					done = true;
+				}else if(input.equals("undo")){
+					DataPoints.undo();
+					System.out.println("Your last entry has been removed");
+				}else if(input.equals("test")){
+					//test case result should be .039x + 65.14
+					DataPoints.useTestData();
+					done = true;
+				}else {
+					parse(input);
+				}
 			}
 		}
-		System.out.println(DataPoints.calculate());
 		
+		System.out.println(DataPoints.calculate());
 		
 
 		JFrame window = new JFrame();
